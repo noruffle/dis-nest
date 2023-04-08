c8a86695ef5943a22fe09062525578a3cbd656dc
Vitalii <44439775+noruffle@users.noreply.github.com>
2023-04-08 20:24:38 +0000

    init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
+  constructor(
+    private service: AuthService
+  ) {}
 
-  
+  @Post('signup')
+  signup() {
+    return this.service.signup()
+  }
+
+  @Post('signin')
+  signin() {
+    return this.service.signin()
+  }
 }
